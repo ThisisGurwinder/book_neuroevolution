a1582ca0548e9f4153e45c57c10fbc7a4e99838b
Gurwinder <ThisisGurwinder@Gmail.com>
2018-01-13 15:50:02 +0530

    Chapter 5

---
+++ chapter_5/README.md
@@ -0,0 +1,8 @@
+***********************************************************
+The encoding of genotype itself can either be direct, or indirect. A direct coding is one in which the genotype encodes every topological
+and parametric aspect of the NN phenotype in one to one manner, the genotype and phenotype can be considered one and the same.An indirect
+encoding applies a set of program, or functions of genotype, through which phenotype is developed.The development process can be highly
+complex and stochasitc in nature which takes into consideration environmental factors during the time of development, and producing a one too
+many mapping from a genotype to phenotype.An example of direct coding is that of a bit string which maps to a colored strip in which 0s are
+directly converted to white sections and 1s to black.An example of indirect coding is example of DNA, where the development from genotype to
+phenotype is multi-staged process, with complex interactions between the developing organism and environment it is in.
---
+++ chapter_5/simple_neuron.erl
@@ -0,0 +1,40 @@
+-module(simple_neuron).
+-compile(export_all).
+
+create() ->
+    Weights = [rand:uniform()-0.5, rand:uniform()-0.5, rand:uniform()-0.5 ],
+    register(neuron, spawn(?MODULE, loop, [Weights])).
+% The create function spawns a single neuron, where bias and weights are generated rand between 0.5 to -0.5
+
+loop(Weights) ->
+    receive
+        {From, Input} ->
+            io:format("**** Processing **** ~n Input: ~p~n Using Weights: ~p ~n", [Input, Weights]),
+            Dot_Product = dot(Input, Weights, 0),
+            Output = [math:tanh(Dot_Product)]),
+            From ! {result, Output},
+            loop(Weights)
+end.
+% The spawned process accepts an Input vector, print it and the weight vector to the screen, calculates the output,
+% and then send the calculated part to the contacting process. The output is also a vector of one.
+
+dot([I|Input], [W|Weights], Acc) ->
+    dot(Input, Weights, I*W+Acc);
+dot([], [Bias], Acc) ->
+    Acc+Bias.
+% The dot product function that we use works on the assumption that the bias is incorporated into the weight list as
+% the last value into lists of weights.After calculating, Input will be empty and Weights list will still have Bias
+% value remaining, which we than add to accumulator.
+
+sense(Signal) ->
+    case is_list(Signal) and (length(Signal) == 2) of
+        true ->
+            neuron ! {self(), Signal},
+            receive
+                {result, Output} ->
+                    io:format(" Output: ~p~n", [Output])
+            end;
+        false ->
+            io:format("The signal must be of length 2 ~n")
+end.
+% We use sense function to contact to neuron and send it to input vector.
